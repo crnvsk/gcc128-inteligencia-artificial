5874c910011b7cab7f1aaf8211d50a35588c2361
Michel Alexandrino de Souza <michelsouza7864@gmail.com>
2025-05-14 21:39:09 -0300

    update: apagando função obsoleta da matriz

--- Projeto-3-Perceptron/metrics.py
+++ Projeto-3-Perceptron/metrics.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 def calculate_metrics(y_true, y_pred):
@@ -10,15 +8,3 @@ def calculate_metrics(y_true, y_pred):
     precision = precision_score(y_true, y_pred, average='weighted')
     recall = recall_score(y_true, y_pred, average='weighted')
     return accuracy, precision, recall
-
-def plot_confusion_matrix(y_true, y_pred, labels, title="Matriz de Confusão"):
-    """
-    Plota a matriz de confusão usando seaborn.
-    """
-    cm = confusion_matrix(y_true, y_pred)
-    plt.figure(figsize=(6, 5))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
-    plt.title(title)
-    plt.xlabel("Previsão")
-    plt.ylabel("Verdadeiro")
-    plt.show()
